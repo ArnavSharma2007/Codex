b87fdc10c2eb62999d04c8a434fbe660701a4a7f
Arnav Sharma <arnav4769.se24@chitkara.edu.in>
2026-04-18 17:32:12 +0530

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   collectCoverageFrom: ['**/*.js', '!**/node_modules/**', '!**/tests/**', '!jest.config.js'],
   coverageThreshold: {
     global: {
