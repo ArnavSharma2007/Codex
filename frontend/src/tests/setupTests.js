c94b824d66df4e9943da7d74e0adc17d9946817c
Arnav Sharma <arnav4769.se24@chitkara.edu.in>
2026-04-18 19:26:28 +0530

    Set up global import meta for testing
    Define global import meta for VITE_BACKEND_URL.

--- frontend/src/tests/setupTests.js
+++ frontend/src/tests/setupTests.js
@@ -1,1 +1,11 @@
+Object.defineProperty(global, 'import', {
+  value: {
+    meta: {
+      env: {
+        VITE_BACKEND_URL: 'http://localhost:5000',
+      },
+    },
+  },
+});
+
 import '@testing-library/jest-dom';
