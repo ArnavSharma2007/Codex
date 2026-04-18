5912410a5f0ebc91b7089c0467c5dbb33837e030
Arnav Sharma <arnav4769.se24@chitkara.edu.in>
2026-04-18 19:30:46 +0530

    Update setupTests.js

--- frontend/src/tests/setupTests.js
+++ frontend/src/tests/setupTests.js
@@ -1,11 +1,1 @@
-Object.defineProperty(global, 'import', {
-  value: {
-    meta: {
-      env: {
-        VITE_BACKEND_URL: 'http://localhost:5000',
-      },
-    },
-  },
-});
-
 import '@testing-library/jest-dom';
