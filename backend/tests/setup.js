13b9b7e99aa534b3d1a1691dfca04000745b0e8e
Arnav Sharma <arnav4769.se24@chitkara.edu.in>
2026-04-18 17:03:18 +0530

    Create setup.js

---
+++ backend/tests/setup.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongo;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log('🧪 In-memory MongoDB connected');
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+  console.log('🛑 In-memory MongoDB stopped');
+});
